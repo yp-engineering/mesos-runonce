2536f4e6c922f6328ab34b359bf08e0f2e27829e
Adam Avilla <aavilla@yp.com>
2016-02-11 16:48:05 -0800

    Adding client.

---
+++ mesosutil/client.go
@@ -0,0 +1,54 @@
+package mesosutil
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+var defaultClient = NewClient()
+
+func NewClient() *Client {
+	c := &Client{}
+
+	c.Transport = NewTransport()
+	c.Client.Transport = c.Transport
+
+	return c
+}
+
+type Client struct {
+	http.Client
+	*Transport
+}
+
+func NewTransport() *Transport {
+	t := *http.DefaultTransport.(*http.Transport)
+
+	return &Transport{
+		Transport: t,
+	}
+}
+
+type Transport struct {
+	http.Transport
+	Debug bool
+}
+
+func (t *Transport) RoundTrip(r *http.Request) (resp *http.Response, err error) {
+	now := time.Now()
+
+	defer func() {
+		if true { // TODO get debug config value
+			if err != nil {
+				log.Printf("[client] transport error %#v\n", err)
+				return
+			}
+			// Time GET / HTTP1/1 200 (2ms)
+			log.Printf("[client] %s %v %s %d (%v)\n",
+				r.Method, r.URL, r.Proto, resp.StatusCode, time.Since(now))
+		}
+	}()
+
+	return t.Transport.RoundTrip(r)
+}
