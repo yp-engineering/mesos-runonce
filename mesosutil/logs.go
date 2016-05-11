01fe3facb6bdfe666a077551a8b520822adec459
Adam Avilla <aavilla@yp.com>
2016-05-10 19:41:48 -0700

    Fix if no domain name is received from mesos.

--- mesosutil/logs.go
+++ mesosutil/logs.go
@@ -96,8 +96,12 @@ func FetchLogs(status *mesos.TaskStatus, offset int, file string) ([]byte, error
 			dir = source
 		}
 	}
-	url := fmt.Sprintf("http://%s.%s:5051/files/read.json?path=%s/%s&offset=%d",
-		firstMtsd.Config.Hostname, firstMtsd.Config.Domainname, dir, file, offset)
+	domainName := ""
+	if firstMtsd.Config.Domainname != "" {
+		domainName = "." + firstMtsd.Config.Domainname
+	}
+	url := fmt.Sprintf("http://%s%s:5051/files/read.json?path=%s/%s&offset=%d",
+		firstMtsd.Config.Hostname, domainName, dir, file, offset)
 	bodyData, _ := FetchLog(url)
 
 	var logData LogData
